93c5e8a5ebfd402daeaf5b2c11b37046fd638740
Janina <85612050+janiweil@users.noreply.github.com>
2021-10-10 14:36:48 +0200

    Prototyp Steuerung Gamepad

--- Assets/Scripts/Prototyp Szene/MovementBoard.cs
+++ Assets/Scripts/Prototyp Szene/MovementBoard.cs
@@ -5,44 +5,49 @@ using UnityEngine;
 public class MovementBoard : MonoBehaviour
 {
     [SerializeField] float speed;
+    [SerializeField] float horizontalRotationSpeed;
+    [SerializeField] float maxTilt = 20f;
+
 
     // Update is called once per frame
     void Update()
     {
-        //InputKeyboard();
-
-        InputController();
         
 
+        InputTilt();
+        InputHorizontalRotation();
+
+
+
     }
 
-    void InputKeyboard()
+    void InputTilt()
     {
-        if (Input.GetKey(KeyCode.W)) //X
-        {
-            this.transform.Rotate(new Vector3(1 * speed * Time.deltaTime, 0, 0), Space.World);
-        }
-        if (Input.GetKey(KeyCode.S)) //Y
-        {
-            this.transform.Rotate(new Vector3(-1 * speed * Time.deltaTime, 0, 0), Space.World);
-        }
+        float xValue = Input.GetAxis("Vertical");
+        float zValue = Input.GetAxis("Horizontal");
 
-        if (Input.GetKey(KeyCode.A)) //Y
+        this.transform.Rotate(new Vector3(xValue * speed * Time.deltaTime, 0, -zValue * speed * Time.deltaTime), Space.World);
+    }
+
+    void TiltLimit() // (funktioniert nicht)
+    {
+        float xRotation = this.transform.rotation.x;
+        float zRotation = this.transform.rotation.z;
+
+        if (Mathf.Abs(xRotation) > 0.2f)
         {
-            this.transform.Rotate(new Vector3(0, 0, 1 * speed * Time.deltaTime), Space.World);
+            this.transform.Rotate(new Vector3(maxTilt * Mathf.Sign(xRotation), 0, 0), Space.World);
         }
-        if (Input.GetKey(KeyCode.D)) //Y
+        if (Mathf.Abs(zRotation) > 0.2f)
         {
-            this.transform.Rotate(new Vector3(0, 0, -1 * speed * Time.deltaTime), Space.World);
+            this.transform.Rotate(new Vector3(0, 0, maxTilt * Mathf.Sign(zRotation)), Space.World);
         }
     }
 
-
-    void InputController()
+    void InputHorizontalRotation()
     {
-         float horizontal = Input.GetAxis("Horizontal"); //-> dreht auf dem "Boden"
-        float vertical = Input.GetAxis("Vertical");
+        float yValue = Input.GetAxis("HorizontalRotation");
 
-        this.transform.Rotate(new Vector3(vertical * speed * Time.deltaTime, 0, -horizontal * speed * Time.deltaTime), Space.World);
+        this.transform.Rotate(new Vector3(0, yValue * horizontalRotationSpeed * Time.deltaTime, 0), Space.World);
     }
 }
