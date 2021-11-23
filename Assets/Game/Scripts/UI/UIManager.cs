11294e5e1d49f52e9e59e5fc79e541e8d84928f2
janiweil <85612050+janiweil@users.noreply.github.com>
2021-11-23 09:07:01 +0100

    ResourceManager: Punkte bei Destroyables

--- Assets/Game/Scripts/UI/UIManager.cs
+++ Assets/Game/Scripts/UI/UIManager.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
+    GameManager gameMng;
 
-  
+    public TMPro.TMP_Text DestroyablePoints;
 
 
     void Start()
     {
-       
+        gameMng = GameManager.Instance;
+
+        gameMng.onDestroyableDestroyed += UpdateDestroyableUI;
     }
    
 
@@ -21,6 +24,12 @@ public class UIManager : MonoBehaviour
 
     }
 
+
+    void UpdateDestroyableUI(float value)
+    {
+        Debug.Log("EventCalled");
+        DestroyablePoints.text = ResourceManager.Instance.DestroyablePoints.ToString();
+    }
 
     // Schreibt hier bitte Methoden, in denen das UI geupdatet wird und Ruft diese dann in den anderen Scripten auf (z.b. über Events), damit das UI nicht permanet unnütig geupdated wird
     // das hier hat ein Singleton pattern, also könnt ihr in anderen Scripten mit UIManager.Instance.methodenname darauf zugreifen
