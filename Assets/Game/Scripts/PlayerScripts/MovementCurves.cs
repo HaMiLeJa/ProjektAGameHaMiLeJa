bd285a52056ddabcf3a8e7669fdd77d871a6a8da
janiweil <85612050+janiweil@users.noreply.github.com>
2021-11-27 17:36:54 +0100

    CurveMovement Working again

--- Assets/Game/Scripts/PlayerScripts/MovementCurves.cs
+++ Assets/Game/Scripts/PlayerScripts/MovementCurves.cs
@@ -14,7 +14,7 @@ public class MovementCurves : MonoBehaviour
     MovementWay movementWay;
 
     Vector3 curveDirection;
-    
+
     Rigidbody rb;
 
     [SerializeField] float speed = 30;
@@ -31,8 +31,8 @@ public class MovementCurves : MonoBehaviour
     {
         if (collision.gameObject.tag == "Curve")
         {
-           if(OnCurve == false)
-           {
+            if (OnCurve == false)
+            {
 
                 OnCurve = true;
 
@@ -41,15 +41,14 @@ public class MovementCurves : MonoBehaviour
 
                 //FindStartpoint (and Direction?)
 
-                //Startpoint reingeben
                 if (waypointCoroutine == null)
-                    waypointCoroutine = StartCoroutine(MoveToWaypoint(startpoint));
+                    waypointCoroutine = StartCoroutine(MoveToWaypoint(0)); //Startpoint reingeben
             }
-                
 
-            
+
+
         }
-       
+
     }
     private void OnCollisionExit(Collision collision)
     {
@@ -67,7 +66,7 @@ public class MovementCurves : MonoBehaviour
     IEnumerator WaitForLeaveCurve()
     {
         RaycastHit hit = new RaycastHit();
-        while(OnCurve == true)
+        while (OnCurve == true)
         {
             if (Physics.Raycast(transform.position, -transform.up, out hit, 20, LayerMask.GetMask("Level")))
             {
@@ -77,7 +76,7 @@ public class MovementCurves : MonoBehaviour
                 }
                 else
                     OnCurve = false;
-                
+
             }
             yield return null;
         }
@@ -86,108 +85,17 @@ public class MovementCurves : MonoBehaviour
         waypointCoroutine = null;
         lastWaypointReached = false;
         StopAllCoroutines();
-        
+
     }
 
-    void FixedUpdate()
-    {
-        /*
-        RaycastHit hit = new RaycastHit();
-        if (Physics.Raycast(transform.position, -transform.up, out hit, 20, LayerMask.GetMask("Level"))) //LayerMask.GetMask("Hex")
-        {
-            if (hit.collider.tag == "Curve")
-            {
-                distanceToCurve = hit.distance;
 
-                if (distanceToCurve <= distance)
-                {
-                    OnCurve = true;
-
-                    if(curveFound == false)
-                    {
-                        curveFound = true;
-                        currentCurve = hit.collider.gameObject.transform.parent.gameObject;
-                        movementWay = currentCurve.GetComponentInChildren<MovementWay>();
-
-
-                    }
-                    
-                }
-                else
-                {
-                    OnCurve = false;
-                }
-            }
-            else
-            {
-                OnCurve = false;
-            }
-
-        }
-        else if (lastWaypointReached == true)
-        {
-            OnCurve = false;
-            curveFound = false;
-
-            lastWaypointReached = false;
-            waypointCoroutine = null;
-        }
-        
-        if(curveFound == true && waypointCoroutine == null)
-        {
-            //Find Startpoint
-            //mov
-
-            if(waypointCoroutine == null)
-                waypointCoroutine = StartCoroutine(MoveToWaypoint(0)); //Startpoint reingeben
-        }
-        
-        */
-    }
-
-    int startpoint;
-    bool listForward = false;
 
     void FindStartpoint()
     {
-        bool findingStartPoint = true;
-        float radius = 10;
-
-        while (findingStartPoint == true)
+        foreach (GameObject waypoint in movementWay.Waypoints)
         {
-            Collider[] hitColliders = Physics.OverlapSphere(this.transform.position, myCollider.radius + radius, LayerMask.GetMask("Waypoints"));
-
-            radius = radius - 0.1f;
-
-            if (hitColliders.Length == 1)
-            {
-                foreach(Collider collider in hitColliders)
-                {
-                    foreach(GameObject waypoint in movementWay.Waypoints)
-                    {
-                        if(collider.gameObject == waypoint)
-                        {
-                            startpoint = movementWay.Waypoints.IndexOf(waypoint);
-                            
-
-                            if(startpoint == 0)
-                            {
-                                listForward = true;
-                            }
-                            else
-                            {
-                                listForward = false;
-                            }
-
-                        }
-                    }
-                }
-
-                findingStartPoint = false;
-
-            }
+            //if player is the nearest bla bla bla
         }
-       
     }
 
 
@@ -200,16 +108,19 @@ public class MovementCurves : MonoBehaviour
     {
         //Debug.Log(point);
         bool waypointReached = false;
-        
+
 
         while (waypointReached == false)
         {
             GameObject waypoint = movementWay.Waypoints[point];
-           
+            //Vector3 direction = waypoint.transform.position / waypoint.transform.position.magnitude;
 
             direction = new Vector3(waypoint.transform.position.x - this.transform.position.x, waypoint.transform.position.y - this.transform.position.y, waypoint.transform.position.z - this.transform.position.z).normalized;
 
             Vector3 movement = direction * Time.deltaTime * speed;
+
+            //rb.MovePosition(this.transform.position + movement);
+
             Vector3 lerpedMovement = new Vector3(Mathf.Lerp(this.transform.position.x, this.transform.position.x + movement.x, 10), Mathf.Lerp(this.transform.position.y, this.transform.position.y + movement.y, 10), Mathf.Lerp(this.transform.position.z, this.transform.position.z + movement.z, 10));
 
             rb.MovePosition(lerpedMovement);
@@ -218,45 +129,40 @@ public class MovementCurves : MonoBehaviour
 
             Collider[] hitColliders;
             hitColliders = Physics.OverlapSphere(this.transform.position, myCollider.radius + 1, LayerMask.GetMask("Waypoints")); //LayerMask.GetMask("World")
-            if(hitColliders != null)
+            if (hitColliders != null)
             {
                 for (int i = 0; i < hitColliders.Length; i++)
                 {
-                    if ( hitColliders[i].gameObject == movementWay.Waypoints[point])
+                    if (hitColliders[i].gameObject == movementWay.Waypoints[point])
                     {
-                       waypointReached = true;
-                       //Debug.Log("waypoint reached");
+                        waypointReached = true;
+                        //Debug.Log("waypoint reached");
                     }
                 }
 
             }
-            
+
             yield return null;
         }
 
 
-        if(listForward == true)
-        {
-            if (point >= movementWay.Waypoints.Count - 1)
-            {
-                lastWaypointReached = true;
 
-            }
-            else
-            {
-                StartCoroutine(MoveToWaypoint(point + 1));
-            }
+        if (point >= movementWay.Waypoints.Count - 1)
+        {
+            lastWaypointReached = true;
+
+        }
+        else
+        {
+            StartCoroutine(MoveToWaypoint(point + 1));
         }
 
-        if(listForward)
-        
-        
 
         yield return null;
     }
 
 
 
-   
+
 
 }
