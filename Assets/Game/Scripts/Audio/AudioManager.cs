cabf1eee9d406ac619a8c7f3dedc06745dcf9d24
janiweil <85612050+janiweil@users.noreply.github.com>
2021-11-27 19:47:15 +0100

    update mainscene

--- Assets/Game/Scripts/Audio/AudioManager.cs
+++ Assets/Game/Scripts/Audio/AudioManager.cs
@@ -3,8 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AudioManager : MonoBehaviour
-{ 
- #region Singleton
+{
+    bool allowAudio = true;
+
+    [SerializeField] bool allowAudioInspector = true;
+
+    private void Start()
+    {
+#if UNITY_EDITOR
+        allowAudio = allowAudioInspector;
+#endif
+    }
+
+    #region Singleton
     public static AudioManager Instance;
    
     private void Awake()
