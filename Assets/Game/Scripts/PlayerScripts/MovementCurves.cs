d99786de3884ebfd7b849dc8e74e7a19bdc5b440
janiweil <85612050+janiweil@users.noreply.github.com>
2021-11-28 11:16:47 +0100

    Auto Movement Curves mit TriggerZones

--- Assets/Game/Scripts/PlayerScripts/MovementCurves.cs
+++ Assets/Game/Scripts/PlayerScripts/MovementCurves.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class MovementCurves : MonoBehaviour
 {
     public bool OnCurve = false;
+
+    public bool StartZoneFound = false;
+    public GameObject StartZone;
+    public GameObject EndZone;
+    [SerializeField] bool listForward = true;
+    [Space]
     [SerializeField] float distanceToCurve;
     [SerializeField] float distance = 3f;
 
@@ -27,8 +33,52 @@ public class MovementCurves : MonoBehaviour
         rb = this.GetComponent<Rigidbody>();
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "CurvesTrigger")
+        {
+            if (StartZone == null)
+            {
+                StartZone = other.gameObject;
+                EndZone = other.GetComponent<TriggerZones>().PartnerZone;
+                MovementCurveStarter();
+            }
+
+            /*
+            if(other.gameObject == EndZone)
+            {
+                StopAllCoroutines();
+                //lastWaypointReached == true
+                rb.AddForce(direction * 50, ForceMode.Impulse);
+
+                OnCurve = false;
+                waypointCoroutine = null;
+                lastWaypointReached = false;
+
+                StartZone = null;
+                EndZone = null;
+            }*/
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject == EndZone)
+        {
+            OnCurve = false;
+            waypointCoroutine = null;
+            lastWaypointReached = false;
+
+            StartZone = null;
+            EndZone = null;
+        }
+    }
+
+    
+
     private void OnCollisionEnter(Collision collision)
     {
+        /*
         if (collision.gameObject.tag == "Curve")
         {
             if (OnCurve == false)
@@ -48,10 +98,12 @@ public class MovementCurves : MonoBehaviour
 
 
         }
+        */
 
     }
     private void OnCollisionExit(Collision collision)
     {
+        /*
         if (collision.gameObject.tag == "Curve")
         {
             if (lastWaypointReached == true)
@@ -60,8 +112,35 @@ public class MovementCurves : MonoBehaviour
                 rb.AddForce(direction * 50, ForceMode.Impulse);
             }
         }
+        */
 
     }
+
+    void MovementCurveStarter()
+    {
+        OnCurve = true;
+        currentCurve = StartZone.transform.parent.gameObject;
+        movementWay = currentCurve.GetComponentInChildren<MovementWay>();
+
+        int startPoint;
+        if (StartZone.GetComponent<TriggerZones>().ZoneNumber == 0)
+        {
+            listForward = true;
+            startPoint = 0;
+        }
+        else
+        {
+            listForward = false;
+            startPoint = movementWay.Waypoints.Count - 1;
+        }
+            
+
+
+        if (waypointCoroutine == null)
+            waypointCoroutine = StartCoroutine(MoveToWaypoint(startPoint)); //Startpoint reingeben
+    }
+
+   
 
     IEnumerator WaitForLeaveCurve()
     {
@@ -89,16 +168,6 @@ public class MovementCurves : MonoBehaviour
     }
 
 
-
-    void FindStartpoint()
-    {
-        foreach (GameObject waypoint in movementWay.Waypoints)
-        {
-            //if player is the nearest bla bla bla
-        }
-    }
-
-
     Coroutine waypointCoroutine;
     public bool lastWaypointReached = false;
     [SerializeField] SphereCollider myCollider;
@@ -113,27 +182,20 @@ public class MovementCurves : MonoBehaviour
         while (waypointReached == false)
         {
             GameObject waypoint = movementWay.Waypoints[point];
-            //Vector3 direction = waypoint.transform.position / waypoint.transform.position.magnitude;
-
+            
             direction = new Vector3(waypoint.transform.position.x - this.transform.position.x, waypoint.transform.position.y - this.transform.position.y, waypoint.transform.position.z - this.transform.position.z).normalized;
-
             Vector3 movement = direction * Time.deltaTime * speed;
-
-            //rb.MovePosition(this.transform.position + movement);
-
             Vector3 lerpedMovement = new Vector3(Mathf.Lerp(this.transform.position.x, this.transform.position.x + movement.x, 10), Mathf.Lerp(this.transform.position.y, this.transform.position.y + movement.y, 10), Mathf.Lerp(this.transform.position.z, this.transform.position.z + movement.z, 10));
 
             rb.MovePosition(lerpedMovement);
 
-
-
-            Collider[] hitColliders;
-            hitColliders = Physics.OverlapSphere(this.transform.position, myCollider.radius + 1, LayerMask.GetMask("Waypoints")); //LayerMask.GetMask("World")
-            if (hitColliders != null)
+            Collider[] waypointCollider;
+            waypointCollider = Physics.OverlapSphere(this.transform.position, myCollider.radius + 1, LayerMask.GetMask("Waypoints")); //LayerMask.GetMask("World")
+            if (waypointCollider != null)
             {
-                for (int i = 0; i < hitColliders.Length; i++)
+                for (int i = 0; i < waypointCollider.Length; i++)
                 {
-                    if (hitColliders[i].gameObject == movementWay.Waypoints[point])
+                    if (waypointCollider[i].gameObject == movementWay.Waypoints[point])
                     {
                         waypointReached = true;
                         //Debug.Log("waypoint reached");
@@ -146,23 +208,37 @@ public class MovementCurves : MonoBehaviour
         }
 
 
-
-        if (point >= movementWay.Waypoints.Count - 1)
+        if (listForward == true)
         {
-            lastWaypointReached = true;
+            Debug.Log("List forward");
+            if (point >= movementWay.Waypoints.Count - 1)
+            {
+                lastWaypointReached = true;
+                rb.AddForce(direction * 50, ForceMode.Impulse);
 
+            }
+            else
+            {
+                StartCoroutine(MoveToWaypoint(point + 1));
+            }
         }
-        else
+
+        if (listForward == false)
         {
-            StartCoroutine(MoveToWaypoint(point + 1));
-        }
+            Debug.Log("List backwards");
 
+            if (point <= 0)
+            {
+                lastWaypointReached = true;
+                rb.AddForce(direction * 50, ForceMode.Impulse);
+            }
+            else
+            {
+                StartCoroutine(MoveToWaypoint(point - 1));
+            }
+        }
 
         yield return null;
     }
-
-
-
-
 
 }
