b37a3754626320d927030acf7d8db4e8dc1906d7
janiweil <85612050+janiweil@users.noreply.github.com>
2021-11-30 22:43:48 +0100

    Vorbereitung Menu

---
+++ Assets/Game/Scenes/Interim/InterimMenuManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InterimMenuManager : MonoBehaviour
+{
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+            Application.Quit();
+    }
+
+    public void OnButtonA()
+    {
+        SceneManager.LoadScene("LevelInterimJanina");
+    }
+
+   
+}
