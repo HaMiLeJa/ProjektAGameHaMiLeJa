c97c1ca64fd1935ddd94c75b8f3f88ff01888e8f
janiweil <85612050+janiweil@users.noreply.github.com>
2022-01-29 17:11:38 +0100

    Level01 Mission Item Pos

--- Assets/Game/Scripts/Missions/MissionStateNoMissionsLeft.cs
+++ Assets/Game/Scripts/Missions/MissionStateNoMissionsLeft.cs
@@ -5,22 +5,13 @@ using UnityEngine;
 public class MissionStateNoMissionsLeft : MonoBehaviour
 {
    
-
-
-    // wv missisonen gab es? wv davon wurden erfüllt?
-    //hexteil gefunden?/Wincon erfüllt?
-
-    //Missionen wiederhohlen + UIMeldung
-
-
-
-    //if mission round 1 = false, direct kopieren und wieder anfangen.
+    
 
 
     public void ReactiveMissions()
     {
-       
 
+        if (MissionManager.MissionRound <= 4) return;
         ReferenceLibary.MissLib.CopyMissionLists();
         ReferenceLibary.MissionMng.SwitchToNoMissionState();
 
