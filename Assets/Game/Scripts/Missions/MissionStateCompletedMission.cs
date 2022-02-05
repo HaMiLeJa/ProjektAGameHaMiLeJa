b134f354f2d37cf953040be675560c7089351494
janiweil <85612050+janiweil@users.noreply.github.com>
2022-02-05 12:18:23 +0100

    neue Kürbistextur (grr, ist das aufwendig)

--- Assets/Game/Scripts/Missions/MissionStateCompletedMission.cs
+++ Assets/Game/Scripts/Missions/MissionStateCompletedMission.cs
@@ -43,7 +43,7 @@ public class MissionStateCompletedMission : MonoBehaviour
     private void UpdateEnergy()
     {
         EnergyManager.energyGotHigher = true;
-        StartCoroutine(ReferenceLibary.EnergyMng.ModifyEnergy(20));
+        StartCoroutine(ReferenceLibary.EnergyMng.ModifyEnergy(50));
     }
 
 
