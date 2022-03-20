38322cbc59997732a4b290b5dd1894f624e91288
Miro Jomaa <s0577239@htw-berlin.de>
2022-03-20 14:23:12 +0100

    drawdistance Einstellungen

--- Assets/Game/Scripts/PlayerScripts/PlayerMovement.cs
+++ Assets/Game/Scripts/PlayerScripts/PlayerMovement.cs
@@ -1,109 +1,63 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using Unity.Mathematics;
-using UnityEngine.Video;
-using UnityEngine.XR;
-
 public class PlayerMovement : MonoBehaviour
 {
     #region Inspector
     [HideInInspector] public Rigidbody rb;
     GameManager gameMng;
     HexMovements hexMov;
-   
-    [Tooltip("Speed with which the player can influence the movement")]
-   // public float StandardMovementSpeed = 10;
-
-    [HideInInspector]public Vector3 MovementDirection;
-
-    // void ControlVelocity
-    public float SlowDownMultiplicator = 0.99f;
-    public float constspeed = 60;
-
+    //[Tooltip("Speed with which the player can influence the movement")] public float StandardMovementSpeed = 10;
+   [HideInInspector]public Vector3 MovementDirection;
+   public float constspeed = 60;
     [SerializeField] private bool constSpeedAllowed = true;
     [HideInInspector] public float originalContspeed;
-// void Basic Jump
-    bool jumping = false;
+    bool jumping;// void Basic Jump
     [SerializeField] float forceJump = 3;
-    float jumpDuration = 0.1f;
-    float timerJump;
-    bool jumpButtonPressedInLastFrame = false;
-    bool allowJump = false;
-
-    [Space]
-    // void GroundCheck und Gravity
-    public bool OnGround = false;
-    float distanceToGround;
+    float jumpDuration = 0.1f, timerJump;
+    bool jumpButtonPressedInLastFrame, allowJump;
+    [Space]    // void GroundCheck und Gravity
+    public bool OnGround;
+    public static float distanceToGround;
     [SerializeField] float fallDownSpeed = 20;
     [Range(0, 10)]
     [SerializeField] float distance = 1.6f;
     [SerializeField] LayerMask hexMask;
     [SerializeField] LayerMask worldMask; //Disabled during shadowdash
     [SerializeField] LayerMask levelMask;
-    public bool DisableGravity = false;
-
+    public bool DisableGravity;
     public Vector3 Velocity; //Debug
-    
     [Space]
     [Header("Max Velocity - Spitzen abfangen")]
     [Tooltip("Was könnte der Spieler maximal erreichen erreichen")]  private float maxSpeed = 700;
-    [Tooltip("Ab wann wird die geschwindigkeit begrenzt (ein klein wenig Kontrollverlust)")] 
-    [SerializeField] private int maxSpeedLimitStartClamping= 600;
+    [Tooltip("Ab wann wird die geschwindigkeit begrenzt (ein klein wenig Kontrollverlust)")] [SerializeField] private int maxSpeedLimitStartClamping= 600;
     [Space]
-    
     public float TotalVelocity;
     [SerializeField] float velocityInfluence = 0.1f;
     [Space]
-
-    // Hight Control
-    private float highControlForce = 5;
+    private float highControlForce = 5;    // Hight Control
     [Tooltip("Choose max Hight")] [Range (10, 60)] [SerializeField] float maxHight = 30;
-
     [Space]
     [SerializeField] AudioSource jumpAudioSource;
-   
-    AudioManager audManager;
-
-
     #endregion
-
     void Start()
     {
         rb = ReferenceLibary.RigidbodyPl;
         gameMng = ReferenceLibary.GameMng;
-        audManager = ReferenceLibary.AudMng;
         hexMov = ReferenceLibary.HexMov;
         originalContspeed = constspeed;
-        //Enable Conspeed!
-        
     }
-
-
     void FixedUpdate()
     {
         if (GameStateManager.gameState == GameStateManager.GameState.Start) return;
-
-        MinVelocity();
-        MaxVelocity();
+        MinVelocity(); MaxVelocity();
         Velocity = rb.velocity; //Debug
-
-        ControlVelocity();
-
-        GroundCheck();
-        Gravity();
-        HightControl();
+        ControlVelocity(); GroundCheck();
+        Gravity(); HightControl();
         TotalVelocity = Mathf.Abs(Velocity.x) + Mathf.Abs(Velocity.y) + Mathf.Abs(Velocity.z);
-
-
-        if (gameMng.AllowMovement == false) return;
-
+        if (!gameMng.AllowMovement) return;
         CalculateMovementDirection();
         //BasicJump();
     }
-
-   
     void MaxVelocity()
     {
         if ((math.abs(rb.velocity.x) + math.abs(rb.velocity.z)) > maxSpeedLimitStartClamping)
@@ -114,8 +68,6 @@ public class PlayerMovement : MonoBehaviour
             rb.velocity = new Vector3(xVelocityMax, rb.velocity.y, zVelocityMax);
         }
     }
-
-     
     void MinVelocity()
     {
         if (!gameMng.AllowMovement) return;
@@ -130,70 +82,34 @@ public class PlayerMovement : MonoBehaviour
             rb.velocity = new Vector3(normalizeSpeed.x * constspeed , rb.velocity.y, normalizeSpeed.z * constspeed);
         }
     }
-    
-    
-    
-    
-    
     Vector3 strafeMovement, forwardMovement;
     void CalculateMovementDirection()
-    {
-        //Bewegung
-         strafeMovement = transform.right * Input.GetAxis("Horizontal");
+    {     //Bewegung
+        strafeMovement = transform.right * Input.GetAxis("Horizontal");
          forwardMovement = transform.forward * Input.GetAxis("Vertical");
          MovementDirection = forwardMovement + strafeMovement; //Richtung, die gerade durch Controller angegeben wird inkl "Eigenen Geschwindigkeit" abhängig von der Stärke der Neigung der Joysticks
         //Vector3 movement = MovementDirection * Time.deltaTime * StandardMovementSpeed;
-
-
-       
-        /*
-        if (shadowDash.currentShadowDashForce != 0f)
-        {
-            rb.AddForce(MovementDirection.normalized * shadowDash.currentShadowDashForce * 50 * Time.fixedDeltaTime);
-
-        }
-        */
-
-
-        //??
+        //  if (shadowDash.currentShadowDashForce != 0f) rb.AddForce(MovementDirection.normalized * shadowDash.currentShadowDashForce * 50 * Time.fixedDeltaTime);
         if (OnGround == false)
         {
             TotalVelocity = Mathf.Abs(Velocity.x) + Mathf.Abs(Velocity.z);
             float velocityPower = TotalVelocity * velocityInfluence/2 * Time.deltaTime;
-
             rb.velocity = (rb.velocity + (MovementDirection * velocityPower));
         }
-
-        /*
-        else
-        {
-            if (rebounded == true) //eig unnötig
-                return;
-
-            totalVelocity = Mathf.Abs(Velocity.x) + Mathf.Abs(Velocity.z);
-            float velocityPower = totalVelocity * velociyInfluence;
-
-            rb.velocity = (rb.velocity + (movement * velocityPower));  // sollte sich bei hoher Geschwindigkeit verstärken ; Wert von ca 5
-
-
-
-            //rb.AddForce(movement, ForceMode.Force);
-        }
-        */
-
+        /*  else
+         {
+             if (rebounded) return; //eig unnötig
+             totalVelocity = Mathf.Abs(Velocity.x) + Mathf.Abs(Velocity.z);
+             float velocityPower = totalVelocity * velociyInfluence;
+             rb.velocity = (rb.velocity + (movement * velocityPower));  // sollte sich bei hoher Geschwindigkeit verstärken ; Wert von ca 5
+             //rb.AddForce(movement, ForceMode.Force);
+         }*/
     }
-
-  
-
     void ControlVelocity()
     {
         if(TotalVelocity > 300) //von total Velocity abhängig machen
-        {
-          rb.velocity = new Vector3(rb.velocity.x * 1.1f, rb.velocity.y, rb.velocity.z * 1.0001f * Time.deltaTime);
-        }
-
+            rb.velocity = new Vector3(rb.velocity.x * 1.1f, rb.velocity.y, rb.velocity.z * 1.0001f * Time.deltaTime);
     }
-
     void BasicJump()
     {
         if (Input.GetButton(gameMng.Jump))
@@ -201,21 +117,15 @@ public class PlayerMovement : MonoBehaviour
             if (OnGround == true && jumpButtonPressedInLastFrame == false) //OnGround == true &&
             {
                 allowJump = true;
-
-                if (jumpAudioSource.isPlaying == false)
-                    jumpAudioSource.Play();
+                if (!jumpAudioSource.isPlaying) jumpAudioSource.Play();
             }
-
             jumpButtonPressedInLastFrame = true;
-
-
-            if (allowJump == true && timerJump < jumpDuration)
+            if (allowJump && timerJump < jumpDuration)
             {
                 jumping = true;
                 timerJump += Time.deltaTime;
                 rb.AddForce(this.transform.up * forceJump * Time.fixedDeltaTime, ForceMode.Impulse);
             }
-
         }
         else
         {
@@ -225,109 +135,53 @@ public class PlayerMovement : MonoBehaviour
             jumping = false;
         }
     }
-
- 
     void HightControl()
     { 
-        if(this.rb.position.y >= maxHight)
-        {
-            rb.AddForce(Vector3.down * highControlForce * rb.transform.position.y * Time.deltaTime);
-
-           //rb.velocity = new Vector3(rb.velocity.x, Mathf.Clamp(rb.velocity.y, -100f, -0.01f ), rb.velocity.y);
-
-            //Idee: stattdessen eine force nach unten? das sollte flüssigeren übergang machen
-
-
+        if(rb.position.y >= maxHight) rb.AddForce(Vector3.down * highControlForce * rb.transform.position.y * Time.deltaTime);
+            //rb.velocity = new Vector3(rb.velocity.x, Mathf.Clamp(rb.velocity.y, -100f, -0.01f ), rb.velocity.y);
+           //Idee: stattdessen eine force nach unten? das sollte flüssigeren übergang machen
             //rb.velocity = new Vector3(rb.velocity.x, Mathf.Lerp(rb.velocity.y, -1 , 0.1f), rb.velocity.y);
-        }
-
     }
-
-
-   
-  
     void GroundCheck()
-    {
-        //GroundControl
-
+    {   //GroundControl
         RaycastHit hit = new RaycastHit();
         if (Physics.Raycast(transform.position, -transform.up, out hit, 10, hexMask)) //LayerMask.GetMask("Hex")
         {
             distanceToGround = hit.distance;
-
-            if (distanceToGround <= distance) //Wert müsste evt über den Spielverlauf hin angepasst werden 1.6
-            {
-                OnGround = true;
-            }
-            else
-            {
-                OnGround = false;
-            }
+            if (distanceToGround <= distance)OnGround = true;//Wert müsste evt über den Spielverlauf hin angepasst werden 1.6
+            else OnGround = false;
         }
-
-        
         else if(Physics.Raycast(transform.position, -transform.up, out hit, 10, worldMask)) //LayerMask.GetMask("World")
         {
             distanceToGround = hit.distance;
-
-            if (distanceToGround <= 1.6f) //Wert müsste evt über den Spielverlauf hin angepasst werden
-            {
-                OnGround = true;
-            }
-            else
-            {
-                OnGround = false;
-            }
+            if (distanceToGround <= 1.6f) OnGround = true; //Wert müsste evt über den Spielverlauf hin angepasst werden
+            else OnGround = false;
         }
         else if(Physics.Raycast(transform.position, -transform.up, out hit, 10, levelMask)) //LayerMask.GetMask("Level")
         {
             distanceToGround = hit.distance;
-
-            if (distanceToGround <= 1.6f) //Wert müsste evt über den Spielverlauf hin angepasst werden
-            {
-                OnGround = true;
-            }
-            else
-            {
-                OnGround = false;
-            }
+            if (distanceToGround <= 1.6f) OnGround = true; //Wert müsste evt über den Spielverlauf hin angepasst werden
+            else OnGround = false;
         }
-        
-       
     }
-
-   void Gravity()
+    void Gravity()
    {
         if (hexMov.OnTrampolinHex) return;
         if (DisableGravity == true) return;
-
         if (OnGround == false && jumping == false) //&&rebounding == false
-        {
             rb.AddForce((rb.velocity.normalized + Vector3.down) * fallDownSpeed, ForceMode.Acceleration);
-        }
         else if (OnGround == false && hexMov.rebounded == false) //Trampolin
-        {
             rb.AddForce((rb.velocity.normalized + Vector3.down) * fallDownSpeed, ForceMode.Acceleration);
-        }
-        
    }
-
-   
-    
-     // private void OnCollisionEnter(Collision collision)
+   // private void OnCollisionEnter(Collision collision)
      // {
           //CollectEnergy
           /*
               if (collision.gameObject.tag == "EnergyGenerator")
               {
                   energyMng.Energy += collision.gameObject.GetComponent<EnergyGenerator>().GeneratedEnergy;
-      
                   collision.gameObject.GetComponent<EnergyGenerator>().GeneratedEnergy = 0;
-      
               }
               */
       //}
-        
 }
-
-
--- Assets/Game/Scripts/WorldScripts/BenderManager.cs
+++ Assets/Game/Scripts/WorldScripts/BenderManager.cs
@@ -1,15 +1,26 @@
 ﻿using System;
 using UnityEngine;
 using UnityEngine.Rendering;
+using NaughtyAttributes;
 [ExecuteAlways]
 public class BenderManager : MonoBehaviour
 {
   private const string BENDER = "ENABLE_BENDING", PLANET = "ENABLE_BENDING_PLANET";
   private static readonly int BENDINGAMOUNT = Shader.PropertyToID("_BendingAmount");
   private bool enablePlanet = true;
-  [SerializeField] [Range(-0.1f, 0.1f)] private float bendingAmount = 0.01f;
   private float _prevAmount;
-  private static float cullingmatrix;
+  private static float cullingMatrixHor , cullingMatrixVert;
+  [InfoBox("Don't touch the Bending Amount if you don't know what you are doing. Default is 0.00988", EInfoBoxType.Warning)]
+  [Tooltip("Changes how much the Planet gets Bend. It is in propotion to the Vert distance from the cameras")]
+  [BoxGroup("Bending")] [SerializeField] [Range(-0.1f, 0.1f)] private float bendingAmount = 0.00988f;
+  [InfoBox("You can change the Frustum Culling Settings here. Do this if you experience Object popping. \n " +
+               "It affects performance greatly by keeping the Frustum Cull tight. Default is 8 | 2 | 4")]
+  [BoxGroup("Frustum Culling")] [Tooltip("Frustum Culls less (higher number) or more (lower number) dependend on the FoV as basenumber")]
+  [Range(-127, 128)] [SerializeField] private float DrawDistance = 8;
+  [BoxGroup("Frustum Culling")] [Tooltip("Adds or substracts a certain amount in the Horizontal Axis compared to the Vertical Axis")]
+  [Range(-63, 64)] [SerializeField] private float HorizontalMod = 2;
+  [BoxGroup("Frustum Culling")] [Tooltip("Takes the distance to the ground and multiplys it by this number. Higher number means less culling when in air")]
+  [Range(0, 15)] [SerializeField] private float HeightMod = 4;
   private void Awake ()
   {
     if ( Application.isPlaying ) Shader.EnableKeyword(BENDER);
@@ -40,14 +51,21 @@ public class BenderManager : MonoBehaviour
     Shader.SetGlobalFloat(BENDINGAMOUNT, bendingAmount);
   }
   private static void OnBeginCameraRendering(ScriptableRenderContext ctx, Camera cam) =>
-    cam.cullingMatrix = Matrix4x4.Ortho(-cullingmatrix, cullingmatrix, -cullingmatrix, cullingmatrix, 0.0001f, 150+cullingmatrix) *
+    cam.cullingMatrix = Matrix4x4.Ortho(-cullingMatrixHor, cullingMatrixHor, -cullingMatrixVert, cullingMatrixVert, 0.0001f, 3*cullingMatrixVert) *
                         cam.worldToCameraMatrix;
   private void UpdateCullingMatrix()
   {
     if (CameraZoomOut.vcamera.m_Lens.FieldOfView < 110)
-      cullingmatrix = 20 + CameraZoomOut.vcamera.m_Lens.FieldOfView;
+    {
+
+      cullingMatrixVert = DrawDistance + PlayerMovement.distanceToGround*HeightMod + CameraZoomOut.vcamera.m_Lens.FieldOfView;
+      cullingMatrixHor = HorizontalMod + cullingMatrixVert;
+    }
     else if (CameraZoomOut.vcamera.m_Lens.FieldOfView >= 110)
-      cullingmatrix = DynamiclyScaleCulling(CameraZoomOut.vcamera);
+    {
+      cullingMatrixVert= PlayerMovement.distanceToGround*HeightMod + DynamiclyScaleCulling(CameraZoomOut.vcamera);
+      cullingMatrixHor = HorizontalMod + cullingMatrixVert * 1.15f;
+    }
   }
   private static float DynamiclyScaleCulling(Cinemachine.CinemachineVirtualCamera vcam) =>
     MathLibary.RemapClamped(110, 180, 140, 260, vcam.m_Lens.FieldOfView);
