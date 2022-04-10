1aa0d298db77af6e5f6b931cdf853581e7cc00bf
Miro Jomaa <s0577239@htw-berlin.de>
2022-04-10 14:35:59 +0200

    small update

--- Assets/Scripts/RenderBounds.cs
+++ Assets/Scripts/RenderBounds.cs
@@ -2,6 +2,8 @@
 using UnityEditor;
 using UnityEngine;
 using System.Collections.Generic;
+using System.Linq;
+
 public class RenderBounds : EditorWindow
 { 
     public static readonly HashSet<MeshFilter> RenderBoundsList = new HashSet<MeshFilter>();
@@ -10,7 +12,7 @@ public class RenderBounds : EditorWindow
     private const float RightBoundMult = 11;
     private static float _multiplicationValue = 1;
     private const string IgnoreLayer = "Hex";
-
+    private static bool noMultButFixedSize;
     [MenuItem("HaMiLeJa/ RenderBounds")]
     public static void ShowWindow()
     {
@@ -32,6 +34,42 @@ public class RenderBounds : EditorWindow
         GUILayout.Space(18);
         if (GUILayout.Button("Draw All Ignore " + IgnoreLayer + " Selected")) AddAllGameObjects();
         GUILayout.Space(4);
+        if (GUILayout.Button("Get Local Bounds")) calculateBoundsParent();
+        GUILayout.Space(4);
+        noMultButFixedSize = EditorGUILayout.Toggle(noMultButFixedSize);
+    }
+    
+
+    void calculateBoundsParent()
+    {
+        foreach (GameObject obj in Selection.gameObjects)
+        {
+            obj.GetComponent<MeshFilter>().sharedMesh.bounds = GetLocalBoundsForObject(obj);
+        }
+    }
+    static Bounds GetLocalBoundsForObject(GameObject go)
+    {
+        var referenceTransform = go.transform;
+        var b = new Bounds(Vector3.zero, Vector3.zero);
+        RecurseEncapsulate(referenceTransform, ref b);
+        return b;
+                       
+        void RecurseEncapsulate(Transform child, ref Bounds bounds)
+        {
+            var mesh = child.GetComponent<MeshFilter>();
+            if (mesh)
+            {
+                var lsBounds = mesh.sharedMesh.bounds;
+                var wsMin = child.TransformPoint(lsBounds.center - lsBounds.extents);
+                var wsMax = child.TransformPoint(lsBounds.center + lsBounds.extents);
+                bounds.Encapsulate(referenceTransform.InverseTransformPoint(wsMin));
+                bounds.Encapsulate(referenceTransform.InverseTransformPoint(wsMax));
+            }
+            foreach (Transform grandChild in child.transform)
+            {
+                RecurseEncapsulate(grandChild, ref bounds);
+            }
+        }
     }
     void AddSelectedToList()
     {
@@ -49,7 +87,7 @@ public class RenderBounds : EditorWindow
         foreach (GameObject showMe in Selection.gameObjects)
         {
             if (showMe.CompareTag("Hex"))
-                foreach (MeshFilter mf in showMe.transform.GetChild(1).GetComponentsInChildren<MeshFilter>()) RenderBoundsList.Add(mf);
+                foreach (MeshFilter mf in showMe.transform.GetComponentsInChildren<MeshFilter>()) RenderBoundsList.Add(mf);
             if (!showMe.CompareTag("Hex"))
             {
                 if (showMe.transform.GetComponent<MeshFilter>() != null)
@@ -64,19 +102,38 @@ public class RenderBounds : EditorWindow
         HexAutoTiling hex = FindObjectOfType<HexAutoTiling>();
         foreach (Transform hasHexTransform in hex.hasAllTheHexGameObjectsTransformsBeforeStart)
         {
-            if (hasHexTransform.GetChild(1).childCount > 0)
-                foreach (MeshFilter mf in hasHexTransform.GetChild(1).GetComponentsInChildren<MeshFilter>())
+            if (hasHexTransform.childCount > 0)
+                foreach (MeshFilter mf in hasHexTransform.GetComponentsInChildren<MeshFilter>())
                 {
-                    if (mf != null) RenderBoundsList.Add(mf);
+                    if (mf != null && mf != hasHexTransform.GetComponent<MeshFilter>()) RenderBoundsList.Add(mf);
                 }
         }
         Debug.Log("You see the Render bounds of : " + RenderBoundsList.Count + " Meshfilters");
     }
     void changeBoundsSize()
     {
+        HashSet<Mesh> uniqueSharedMeshes = new HashSet<Mesh>();
+
+        foreach (MeshFilter mf in RenderBoundsList)
+        {
+            uniqueSharedMeshes.Add(mf.sharedMesh);
+        }
+
+        List <Mesh> uniqueSharedMeshesList = uniqueSharedMeshes.ToList();
+        
+        if (Selection.gameObjects.Length == 0)
+        {
+            AddAllGameObjects();
+            foreach (Mesh mesh in uniqueSharedMeshesList)
+            {
+                mesh.RecalculateBounds();
+                mesh.bounds = new Bounds(mesh.bounds.center, new Vector3(mesh.bounds.size.x * _multiplicationValue, mesh.bounds.size.y, mesh.bounds.size.z * _multiplicationValue));
+            }
+        }
+        
         foreach (GameObject showMe in Selection.gameObjects)
         {
-            if (showMe.CompareTag("Hex")) foreach (MeshFilter mf in showMe.transform.GetChild(1).GetComponentsInChildren<MeshFilter>()) HasAllBoundsToModify.Add(mf);
+            if (showMe.CompareTag("Hex")) foreach (MeshFilter mf in showMe.transform.GetComponentsInChildren<MeshFilter>()) HasAllBoundsToModify.Add(mf);
             if (!showMe.CompareTag("Hex"))
             {
                 if (showMe.transform.GetComponent<MeshFilter>() != null) RenderBoundsList.Add(showMe.transform.GetComponent<MeshFilter>());
@@ -92,10 +149,28 @@ public class RenderBounds : EditorWindow
     }
     void recalculateBoundsSize()
     {
+        HashSet<Mesh> uniqueSharedMeshes = new HashSet<Mesh>();
+
+        foreach (MeshFilter mf in RenderBoundsList)
+        {
+            uniqueSharedMeshes.Add(mf.sharedMesh);
+        }
+
+        List <Mesh> uniqueSharedMeshesList = uniqueSharedMeshes.ToList();
+        
+        if (Selection.gameObjects.Length == 0)
+        {
+            AddAllGameObjects();
+            foreach (Mesh mesh in uniqueSharedMeshesList)
+            {
+                mesh.RecalculateBounds();
+            }
+        }
+        
         foreach (GameObject showMe in Selection.gameObjects)
         {
             if (showMe.CompareTag("Hex")) 
-                foreach (MeshFilter mf in showMe.transform.GetChild(1).GetComponentsInChildren<MeshFilter>()) HasAllBoundsToModify.Add(mf);
+                foreach (MeshFilter mf in showMe.transform.GetComponentsInChildren<MeshFilter>()) HasAllBoundsToModify.Add(mf);
             if (!showMe.CompareTag("Hex"))
             {
                 if (showMe.transform.GetComponent<MeshFilter>() != null) RenderBoundsList.Add(showMe.transform.GetComponent<MeshFilter>());
